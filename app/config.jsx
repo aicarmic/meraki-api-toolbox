410f8e781d034d0ee915f9c8e98c20219243a6f0
Tony Carmichael <ancarmic@cisco.com>
2019-11-17 17:02:26 -0800

    Revert "fix paths"
    This reverts commit 5b742d48c5590275f1d14e699ebc8604abdcb6cb.

--- app/config.jsx
+++ app/config.jsx
@@ -37,11 +37,11 @@ export default class Config extends React.Component{
     api_key_textbox() {
         return (
             <FormGroup controlId="api_key_text">
-                <ControlLabel>API Key</ControlLabel>
+                <ControlLabel>Api Key</ControlLabel>
                 <FormControl
                     type="text"
                     value={this.state.api_key}
-                    placeholder="Enter API key"
+                    placeholder="Enter Api key"
                     onChange={this.update_api_key}
                 />
             </FormGroup>
